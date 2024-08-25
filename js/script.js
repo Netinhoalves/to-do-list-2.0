7861c01222bf2662f0547377ac78d6a12ab04964
netinhoalves <netinhoalves_2011@hotmail.com>
2024-08-25 13:09:07 -0400

    função e evento do filtro feita

--- js/script.js
+++ js/script.js
@@ -8,10 +8,11 @@ const toolbar = document.querySelector('#toolbar');
 const searchInput = document.querySelector('#search-input');
 const eraseButton = document.querySelector('#erase-button');
 const filterSelect = document.querySelector('#filter-select');
-const todos = document.querySelectorAll('.todo');
 
 // Funções
 const searchTodos = (searchValue) => {
+  const todos = document.querySelectorAll('.todo');
+
   todos.forEach((todo) => {
     const todoTitle = todo.querySelector('h5').innerText.toLowerCase();
 
@@ -19,6 +20,60 @@ const searchTodos = (searchValue) => {
       todo.style.display = 'flex';
     } else {
       todo.style.display = 'none';
+    }
+  });
+};
+
+const filterTodos = (filter) => {
+  const todos = document.querySelectorAll('.todo');
+
+  todos.forEach((todo) => {
+    switch (filter) {
+      case 'all':
+        todo.style.display = 'flex';
+        break;
+      case 'done':
+        todo.classList.contains('done')
+          ? (todo.style.display = 'flex')
+          : (todo.style.display = 'none');
+        break;
+      case 'todo':
+        !todo.classList.contains('done')
+          ? (todo.style.display = 'flex')
+          : (todo.style.display = 'none');
+        break;
+      case 'low':
+        todo.querySelector('.priority.low')
+          ? (todo.style.display = 'flex')
+          : (todo.style.display = 'none');
+        break;
+      case 'medium':
+        todo.querySelector('.priority.medium')
+          ? (todo.style.display = 'flex')
+          : (todo.style.display = 'none');
+        break;
+      case 'high':
+        todo.querySelector('.priority.high')
+          ? (todo.style.display = 'flex')
+          : (todo.style.display = 'none');
+        break;
+      case 'house':
+        todo.querySelector('.card-img-top.house')
+        ? (todo.style.display = 'flex')
+        : (todo.style.display = 'none');
+      break;
+      case 'education':
+        todo.querySelector('.card-img-top.education')
+        ? (todo.style.display = 'flex')
+        : (todo.style.display = 'none');
+      break;
+      case 'work':
+        todo.querySelector('.card-img-top.work')
+        ? (todo.style.display = 'flex')
+        : (todo.style.display = 'none');
+      break;
+      default:
+        todo.style.display = 'flex';
     }
   });
 };
@@ -33,4 +88,9 @@ eraseButton.addEventListener('click', (e) => {
   e.preventDefault();
 
   searchInput.value = '';
+  searchTodos('');
+});
+
+filterSelect.addEventListener('change', (e) => {
+  filterTodos(e.target.value);
 });
